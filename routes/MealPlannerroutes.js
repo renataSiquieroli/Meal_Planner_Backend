ae563461512b9eeccceaa53a852999448301772c
Renata Siquieroli <resiquieroli@gmail.com>
2023-10-24 11:39:55 +0200

    file name

--- routes/MealPlannerroutes.js
+++ routes/MealPlannerroutes.js
@@ -1,19 +1,20 @@
 const express = require("express");
 // import require middleware
-const { createMealPlanner,
-    getAllMealPlanners,
-    getMealPlannerById,
-    updateMealPlanner,
-    deleteMealPlanner } = require("../controller/mealPlannerController");
+const {
+  createMealPlanner,
+  getAllMealPlanners,
+  getMealPlannerById,
+  updateMealPlanner,
+  deleteMealPlanner,
+} = require("../controller/mealPlannerController");
 
 const app = express.Router();
 //use auth here
 
-app.post('/create-meal-planners', createMealPlanner);
-app.get('/getall-meal-planners', getAllMealPlanners);
-app.get('/getbyid-meal-planners/:id',getMealPlannerById);
-app.put('/update-meal-planners/:id', updateMealPlanner);
-app.delete('/delete-meal-planners/:id', deleteMealPlanner);
-
+app.post("/create-meal-planners", createMealPlanner);
+app.get("/getall-meal-planners", getAllMealPlanners);
+app.get("/getbyid-meal-planners/:id", getMealPlannerById);
+app.put("/update-meal-planners/:id", updateMealPlanner);
+app.delete("/delete-meal-planners/:id", deleteMealPlanner);
 
 module.exports = app;
